963093e3e570f53d32e08e1cd897289d9233f0d1
Jorge Cortes <jcortes@aviatrix.com>
2023-08-29 15:12:03 +0000

    specified python3 subversion

--- modules/aviatrix_controller_initialize/main.tf
+++ modules/aviatrix_controller_initialize/main.tf
@@ -25,6 +25,6 @@ locals {
 }
 resource "null_resource" "run_script" {
   provisioner "local-exec" {
-    command = "python3 -W ignore ${local.option} ${local.argument}"
+    command = "python3.9 -W ignore ${local.option} ${local.argument}"
   }
 }
